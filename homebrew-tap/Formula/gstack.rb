b1051d8b687daf7bcb201d70a0c516c66be130fa
Nicolas Gaviria <nicolasgaviria94@gmail.com>
2025-12-30 01:07:01 -0500

    RElease

--- homebrew-tap/Formula/gstack.rb
+++ homebrew-tap/Formula/gstack.rb
@@ -1,13 +1,13 @@
 class Gstack < Formula
   desc "CLI tool for managing stacked Git branches with automated rebasing"
   homepage "https://github.com/nicomalacho/stack-branch"
-  version "0.2.0"
+  version "0.3.0"
   license "MIT"
 
   on_macos do
     # ARM64 binary - Intel Macs can run via Rosetta 2
     url "https://github.com/nicomalacho/stack-branch/releases/download/v#{version}/gs-macos-arm64"
-    sha256 "bf09715efbd8a6f646e4a6e3b363aae70125c69b28e98e69401fffda623e3f2b"
+    sha256 "457330efabe2b954d8a1c1ae8f8c21ea0a007a0272ef5774a8e4081bc715dd0e"
 
     def install
       bin.install "gs-macos-arm64" => "gs"
@@ -16,7 +16,7 @@ class Gstack < Formula
 
   on_linux do
     url "https://github.com/nicomalacho/stack-branch/releases/download/v#{version}/gs-linux-x86_64"
-    sha256 "c6f55f1dc23359b99c3a34d206158fd9f5878b795ddb89fcf7a337da9156d345"
+    sha256 "0849f026f0ce8594215ab728c43cc879687b018fc6f88f9f07671a1f8deb9d1e"
 
     def install
       bin.install "gs-linux-x86_64" => "gs"
